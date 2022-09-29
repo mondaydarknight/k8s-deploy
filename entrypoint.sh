30157d6e3636b9c15517852ad5607c9cbcff111f
mongche <mondaydarknight@gmail.com>
2022-09-29 18:52:25 +0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ kubectl config set-credentials ${INPUT_USER} --token=${INPUT_TOKEN}
 kubectl config set-context ${INPUT_CONTEXT} --cluster=${INPUT_CLUSTER} --user=${INPUT_USER}
 kubectl config use-context ${INPUT_CONTEXT}
 
-cd ${TEMPLATE_PATH}
+cd ${INPUT_TEMPLATE_PATH}
 kubectl cluster-info
 kubectl config get-contexts
 kubectl config view
