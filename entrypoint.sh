e50bb4e0df17a46eb5b89a3d98db0274efc17834
mongche <mondaydarknight@gmail.com>
2022-09-29 18:36:23 +0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,7 @@ kubectl config set-cluster ${INPUT_CLUSTER} --server=${INPUT_SERVER} --insecure-
 kubectl config set-credentials ${INPUT_USER} --token=${INPUT_TOKEN}
 kubectl config set-context ${INPUT_CONTEXT} --cluster=${INPUT_CLUSTER} --user=${INPUT_USER}
 kubectl config use-context ${INPUT_CONTEXT}
+kubectl cluster-info
+kubectl config get-contexts
+kubectl config view
 echo "::endgroup::"
