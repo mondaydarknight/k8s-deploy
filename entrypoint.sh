2edba70302344ca07fe37616b5d26905cdcb60d2
mongche <mondaydarknight@gmail.com>
2022-10-01 14:18:37 +0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -euxo pipefail
 
-echo "::group::Setup K8S cluster"
+echo "::group::Access K8S cluster"
 kubectl config set-cluster ${INPUT_CLUSTER} --server=${INPUT_SERVER} --insecure-skip-tls-verify=true
 kubectl config set-credentials ${INPUT_USER} --token=${INPUT_TOKEN}
 kubectl config set-context ${INPUT_CONTEXT} --cluster=${INPUT_CLUSTER} --user=${INPUT_USER}
@@ -9,7 +9,7 @@ kubectl config use-context ${INPUT_CONTEXT}
 echo "::endgroup::"
 
 echo "::group::Deploy K8S cluster"
-ls -al
+[[ ${INPUT_VALIDATED} == 'true' ]] && K8S_ARGS='--validate=true --dry-run=client'
 cd ${INPUT_TEMPLATE_PATH}
-kubectl apply -k .
+kubectl apply ${K8S_ARGS} -k .
 echo "::endgroup::"
